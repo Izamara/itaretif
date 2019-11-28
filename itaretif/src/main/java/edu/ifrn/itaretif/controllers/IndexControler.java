e7faf3aeeaff52e89214eab61212e2b7d141a428
Ana Sara <ana.sara1417@gmail.com>
2019-11-28 17:20:28 -0200

    Criando página de index.

---
+++ itaretif/src/main/java/edu/ifrn/itaretif/controllers/IndexControler.java
@@ -0,0 +1,13 @@
+package edu.ifrn.itaretif.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexControler {
+	
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+}
