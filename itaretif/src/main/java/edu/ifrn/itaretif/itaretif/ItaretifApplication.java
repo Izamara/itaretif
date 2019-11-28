f6c9f5c14f10c2c32bd2065195a1dc3174f33903
Izamara <isafaustino009@gmail.com>
2019-11-28 16:20:10 -0200

    Criando o projeto do ItaretIF

---
+++ itaretif/src/main/java/edu/ifrn/itaretif/itaretif/ItaretifApplication.java
@@ -0,0 +1,13 @@
+package edu.ifrn.itaretif.itaretif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItaretifApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItaretifApplication.class, args);
+	}
+
+}
