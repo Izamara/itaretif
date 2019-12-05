a63d522e57162d27be815c05390022666a6aa947
20161204010076 <20161204010076@LAJ291354.ifrn.local>
2019-12-05 18:37:40 -0200

    Adicionando o Repository do usuário.

---
+++ itaretif/src/main/java/edu/ifrn/itaretif/repositories/UsuarioRepository.java
@@ -0,0 +1,15 @@
+package edu.ifrn.itaretif.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import edu.ifrn.itaretif.models.Usuario;
+
+@Repository
+public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
+
+	Usuario finByMatricula(String matricula);
+	
+	Iterable<Usuario> findByNomeContaining(String nome);
+	
+}
