0b84b4c98d088f2e619fef5630db140e221bcf8d
Ana Sara <ana.sara1417@gmail.com>
2019-12-06 12:58:12 -0200

    Criação da aba de notícias.

--- itaretif/src/main/java/edu/ifrn/itaretif/models/Noticias.java
+++ itaretif/src/main/java/edu/ifrn/itaretif/models/Noticias.java
@@ -1,7 +1,15 @@
 package edu.ifrn.itaretif.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Noticias {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String titulo;
 	private String conteudo;
