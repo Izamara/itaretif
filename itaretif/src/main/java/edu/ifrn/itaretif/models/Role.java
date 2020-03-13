762686252f0cb42f72d14fa3d6e6f1c9e91d296d
Nanikka <malu03silva@gmail.com>
2020-03-13 17:49:34 -0300

    Alterações no código para solucionar o problema do login. Mudança no retorno de um método.

--- itaretif/src/main/java/edu/ifrn/itaretif/models/Role.java
+++ itaretif/src/main/java/edu/ifrn/itaretif/models/Role.java
@@ -1,7 +1,10 @@
 package edu.ifrn.itaretif.models;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 import org.springframework.security.core.GrantedAuthority;
 
@@ -10,16 +13,42 @@ public class Role implements GrantedAuthority {
 
 	@Id
 	private Long id;
-
+	
 	private String nomeRole;
 
+	@ManyToMany(mappedBy = "roles")
+	private List <Usuario> usuarios;
+	
+	
 	public String getNomeRole() {
 		return nomeRole;
 	}
 
+
 	public void setNomeRole(String nomeRole) {
 		this.nomeRole = nomeRole;
 	}
+	
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public List<Usuario> getUsuarios() {
+		return usuarios;
+	}
+
+
+	public void setUsuarios(List<Usuario> usuarios) {
+		this.usuarios = usuarios;
+	}
+
 
 	@Override
 	public String getAuthority() {
@@ -27,12 +56,12 @@ public class Role implements GrantedAuthority {
 		return this.nomeRole;
 	}
 
-	public Long getId() {
-		return id;
-	}
 
-	public void setId(Long id) {
-		this.id = id;
+	@Override
+	public String toString() {
+		return "Role [id=" + id + ", nomeRole=" + nomeRole + "]";
 	}
-
+	
+	
+	
 }
