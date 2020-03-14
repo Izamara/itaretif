a1747bbdf1107b1665096ce50c009c2ed00b2855
Nanikka <malu03silva@gmail.com>
2020-03-14 18:28:14 -0300

    Declarando String nome.

--- itaretif/src/main/java/edu/ifrn/itaretif/models/Usuario.java
+++ itaretif/src/main/java/edu/ifrn/itaretif/models/Usuario.java
@@ -21,6 +21,8 @@ public class Usuario implements UserDetails {
 
 	@Id
 	private Long id;
+	
+	private String nome;
 
 	private String matricula;
 
@@ -37,6 +39,15 @@ public class Usuario implements UserDetails {
 
 	public void setId(Long id) {
 		this.id = id;
+	}
+	
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
 
 	public String getMatricula() {
