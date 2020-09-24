1bdc49c0ef813dbee9892ec124f46e623efc60ab
Nanikka <malu03silva@gmail.com>
2020-09-24 13:02:49 -0300

    Verificação de erro com Fernando (Adição do Sysout).

--- itaretif/src/main/java/edu/ifrn/itaretif/ItaretifApplication.java
+++ itaretif/src/main/java/edu/ifrn/itaretif/ItaretifApplication.java
@@ -2,7 +2,6 @@ package edu.ifrn.itaretif;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
