51331eb7ebb588e3e37b0c595f667c11b90f76e9
Nanikka <malu03silva@gmail.com>
2021-02-20 17:30:29 -0300

    Exclusão da interface "NoticiasRepository" e da págiina "inicioServidor".

--- itaretif/src/main/java/edu/ifrn/itaretif/repositories/NoticiasRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.ifrn.itaretif.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import edu.ifrn.itaretif.models.Noticias;
-
-public interface NoticiasRepository extends CrudRepository<Noticias, String>{
-
-}
