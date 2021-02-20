30d7252a8cf4ad554e3a3cd58a5d27188b13bd8e
Nanikka <malu03silva@gmail.com>
2021-02-20 17:31:57 -0300

    Mudança no retorno do método do controller "CadastroContoller".

--- itaretif/src/main/java/edu/ifrn/itaretif/controllers/CadastroController.java
+++ itaretif/src/main/java/edu/ifrn/itaretif/controllers/CadastroController.java
@@ -1,38 +1,16 @@
 package edu.ifrn.itaretif.controllers;
 
-import java.util.ArrayList;
-import java.util.StringTokenizer;
-
-import javax.validation.Valid;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import edu.ifrn.itaretif.models.Role;
-import edu.ifrn.itaretif.models.Usuario;
-import edu.ifrn.itaretif.repository.RoleRepository;
-import edu.ifrn.itaretif.repository.UsuarioRepository;
 
 @Controller
 public class CadastroController {
 	
-	@Autowired
-	private UsuarioRepository ur;
-	
-	@Autowired
-	private RoleRepository rr;
-
-	
 	@RequestMapping(value="/cadastro", method=RequestMethod.GET)
 		public String formCadastro(){
 		
-		return "paginaDeCadastro";
+		return "/cadastro/paginaDeCadastro";
 	}
 
 	
